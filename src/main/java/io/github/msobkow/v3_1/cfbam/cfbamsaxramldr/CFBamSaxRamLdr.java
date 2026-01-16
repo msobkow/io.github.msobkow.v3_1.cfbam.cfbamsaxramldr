d4f3754d88c12b2e22227b29aa8c4e8edd893e6f
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 05:37:03 -0600

    2026-01-16 05:35 I believe the remapping of packages from server.markhome.mcf to io.github.msobkow is complete.

---
+++ src/main/java/io/github/msobkow/v3_1/cfbam/cfbamsaxramldr/CFBamSaxRamLdr.java
@@ -0,0 +1,138 @@
+// Description: Java 25 XML SAX Parser for CFBam.
+
+/*
+ *	io.github.msobkow.CFBam
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal 3.1 CFBam - Business Application Model
+ *	
+ *	This file is part of Mark's Code Fractal CFBam.
+ *	
+ *	Mark's Code Fractal CFBam is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU General Public License,
+ *	Version 3 or later.
+ *	
+ *	Mark's Code Fractal CFBam is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFBam is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with Mark's Code Fractal CFBam.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ *	
+ */
+
+package io.github.msobkow.v3_1.cfbam.cfbamsaxramldr;
+
+import org.apache.log4j.*;
+import io.github.msobkow.v3_1.cflib.*;
+import io.github.msobkow.v3_1.cflib.xml.*;
+import io.github.msobkow.v3_1.cfsec.cfsec.*;
+import io.github.msobkow.v3_1.cfint.cfint.*;
+import io.github.msobkow.v3_1.cfbam.cfbam.*;
+
+import io.github.msobkow.v3_1.cfsec.cfsecobj.*;
+import io.github.msobkow.v3_1.cfint.cfintobj.*;
+import io.github.msobkow.v3_1.cfbam.cfbamobj.*;
+import io.github.msobkow.v3_1.cfbam.CFBamRam.*;
+import io.github.msobkow.v3_1.cfbam.CFBamSaxLoader.*;
+
+public class CFBamSaxRamLdr
+	extends CFBamSaxLdr
+{
+	private static ICFLibMessageLog log = new CFLibConsoleMessageLog();
+
+	// Constructors
+
+	public CFBamSaxRamLdr() {
+		super( log );
+	}
+
+	// main() entry point
+
+	public static void main( String args[] ) {
+		final String S_ProcName = "CFBamSaxRamLdr.main() ";
+		initConsoleLog();
+		int numArgs = args.length;
+		if( numArgs >= 2 ) {
+			ICFBamSchema cFBamSchema = new CFBamRamSchema();
+			ICFBamSchemaObj cFBamSchemaObj = new CFBamSchemaObj();
+			cFBamSchemaObj.setBackingStore( cFBamSchema );
+			CFBamSaxLdr cli = new CFBamSaxRamLdr();
+			CFBamSaxLoader loader = cli.getSaxLoader();
+			loader.setSchemaObj( cFBamSchemaObj );
+			String url = args[1];
+			try {
+				cFBamSchema.connect( "system", "system", "system", "system" );
+				cFBamSchema.rollback();
+				cFBamSchema.beginTransaction();
+				cFBamSchemaObj.setSecCluster( cFBamSchemaObj.getClusterTableObj().getSystemCluster() );
+				cFBamSchemaObj.setSecTenant( cFBamSchemaObj.getTenantTableObj().getSystemTenant() );
+				cFBamSchemaObj.setSecSession( cFBamSchemaObj.getSecSessionTableObj().getSystemSession() );
+				CFSecAuthorization auth = new CFSecAuthorization();
+				auth.setSecCluster( cFBamSchemaObj.getSecCluster() );
+				auth.setSecTenant( cFBamSchemaObj.getSecTenant() );
+				auth.setSecSession( cFBamSchemaObj.getSecSession() );
+				cFBamSchemaObj.setAuthorization( auth );
+				loader.setUseCluster( cFBamSchemaObj.getSecCluster() );
+				loader.setUseTenant( cFBamSchemaObj.getSecTenant() );
+				applyLoaderOptions( loader, args[0] );
+				cli.evaluateRemainingArgs( args, 2 );
+				loader.parseFile( url );
+				cFBamSchema.commit();
+				cFBamSchema.disconnect( true );
+			}
+			catch( Exception e ) {
+				log.message( S_ProcName + "EXCEPTION: Could not parse XML file \"" + url + "\": " + e.getMessage() );
+				e.printStackTrace( System.out );
+			}
+			catch( Error e ) {
+				log.message( S_ProcName + "ERROR: Could not parse XML file \"" + url + "\": " + e.getMessage() );
+				e.printStackTrace( System.out );
+			}
+			finally {
+				if( cFBamSchema.isConnected() ) {
+					cFBamSchema.rollback();
+					cFBamSchema.disconnect( false );
+				}
+			}
+		}
+		else {
+			log.message( S_ProcName + "ERROR: Expected at least two argument specifying the loader options and the name of the XML file to parse.  The first argument may be empty." );
+		}
+	}
+
+	// Initialize the console log
+
+	protected static void initConsoleLog() {
+//		Layout layout = new PatternLayout(
+//				"%d{ISO8601}"		// Start with a timestamp
+//			+	" %-5p"				// Include the severity
+//			+	" %C.%M"			// pkg.class.method()
+//			+	" %F[%L]"			// File[lineNumber]
+//			+	": %m\n" );			// Message text
+//		BasicConfigurator.configure( new ConsoleAppender( layout, "System.out" ) );
+	}
+
+	// Evaluate remaining arguments
+
+	public void evaluateRemainingArgs( String[] args, int consumed ) {
+		// There are no extra arguments for the RAM "database" instance
+		if( args.length > consumed ) {
+			log.message( "CFBamSaxRamLdr.evaluateRemainingArgs() WARNING No extra arguments are expected for a RAM database instance, but "
+				+ Integer.toString( args.length - consumed )
+				+ " extra arguments were specified.  Extra arguments ignored." );
+		}
+	}
+
+}
